b867d2042c18de439edd35ef977553e876968f1c
JakesProjects1 <jacob@kagon.net>
2021-03-21 20:23:28 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-const Index = () => {
-  return <div>Hello World</div>;
-};
+import React from "react";
 
-export default Index
+
+
+export default class Index extends React.Component {
+  async initializeAppFunctions() {
+    const admin = require("firebase-admin");
+
+    const serviceAccount = require("../serviceAccountKey.json");
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+
+    const db = admin.firestore();
+  }
+
+  // static async getInitialProps() {
+  //   let firebase = await loadFirebase();
+  //   let db = firebase.firestore();
+  //   const snapshot = await db.collection("Food").get();
+  //   snapshot.forEach((doc) => {
+  //     console.log(doc.id, "=>", doc.data());
+  //   });
+  // }
+
+  render() {
+    return <div>{getInitialProps()}Hello World</div>;
+  }
+}
